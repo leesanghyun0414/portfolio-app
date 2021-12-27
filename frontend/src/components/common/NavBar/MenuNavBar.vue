52c647dbc6bad3ff351242d668dd86bf68e9a9e9
rinnmooon <kojurinnmymelo@gmail.com>
2021-12-27 21:28:19 +0900

    wantfix

---
+++ frontend/src/components/common/NavBar/MenuNavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    
+
+
+</template>
+
+
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useQuery, useResult } from "@vue/apollo-composable"
+import gql from "graphql-tag"
+import { GET_ALLCATEGORY } from "../../../api/graphql/querys"
+import { watch } from "vue"
+
+export default defineComponent({
+   name: "MenuNavBar",
+   
+// TODO: set up 追加
+   setup() {
+
+const { result } = useQuery(GET_ALLCATEGORY)
+
+return { 
+   result,
+}
+
+watch(result, value => {
+      console.log(GET_ALLCATEGORY)
+   
+   
+   }
+
+
+</script>
+
---
+++ frontend/src/components/common/NavBar/NavBarList.vue
@@ -0,0 +1,22 @@
+<template>
+
+
+
+</template>
+
+
+<script>
+import { defineComponent } from "vue"
+import NavBarList from "./NavBarList.vue"
+
+    export default defineComponent({
+name: "NavBarList",
+
+
+
+
+
+
+    })
+
+</script>
