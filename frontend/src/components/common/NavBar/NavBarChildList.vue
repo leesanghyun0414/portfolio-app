10c8e9f656b25e609809add5913e1b592669a800
s <tsuyuharu414@gmail.com>
2022-01-04 20:56:33 +0900

    Imlements App Navbar

---
+++ frontend/src/components/common/NavBar/NavBarChildList.vue
@@ -0,0 +1,15 @@
+<template>
+$END$
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+name: "NavBarChildList"
+})
+</script>
+
+<style scoped>
+
+</style>
