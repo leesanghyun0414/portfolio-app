34f52122ccbedd5682de89b1ab8fe2feee78c65f
s <tsuyuharu414@gmail.com>
2022-03-13 18:03:53 +0900

    ルートimport パス変更

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -2,9 +2,8 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
 import MapPage from "../pages/MapPage.vue"
 import HomePage from "../pages/HomePage.vue"
 import MenuPage from "../pages/MenuPage.vue"
-import MenuCategory from "../pages/MenuCategory.vue"
+import MenuCategory from "../components/menu_category/MenuCategory.vue"
 import Menu from "../components/menu/Menu.vue"
-import InformationPage from "../pages/InformationPage.vue"
 
 const routes: RouteRecordRaw[] = [
   { path: "/", component: HomePage, name: "HomePage" },
