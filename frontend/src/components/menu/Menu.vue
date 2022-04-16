9289d267db92f90d7d7e064f2a940302ee0d8687
sanghyun.lee <lee.sanghyun0414@gmail.com>
2022-04-16 20:38:36 +0900

    for test

--- frontend/src/components/menu/Menu.vue
+++ frontend/src/components/menu/Menu.vue
@@ -1,36 +1,36 @@
-<script setup lang="ts">
-import { useQuery, useResult } from "@vue/apollo-composable"
-import { GET_MENU_BY_CATEGORY_ID } from "../../api/graphql/querys"
-
-const menus = useQuery(GET_MENU_BY_CATEGORY_ID, )
-
-</script>
-
 <template>
-  <p>{{ menuName }}</p>
+  <p v-if="!loading">
+    {{ menuName }}
+  </p>
+  <p>{{ menus }}</p>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
 
+import { GET_MENU_BY_CATEGORY_ID } from "../../api/graphql/querys"
+import { useQuery, useResult } from "@vue/apollo-composable"
+
 export default defineComponent({
   name: "Menu",
   props: {
-    menuName :{
+    menuName: {
       type: String,
       required: true
     },
-    categoryId : {
-      type: Number,
+    categoryId: {
+      type: String,
       required: true
     }
   },
   setup(props) {
-    console.log(props.menuName)
+    console.log(props.categoryId)
+    const {result, loading} = useQuery(GET_MENU_BY_CATEGORY_ID, { categoryId: props.categoryId })
+    const menus = useResult(result, null, (data) => data.menuByCategoryId)
+  return {
+      loading,
+      menus
+  }
   }
 })
 </script>
-
-<style scoped>
-
-</style>
--- frontend/src/components/menu_category/MenuCategoryItem.vue
+++ frontend/src/components/menu_category/MenuCategoryItem.vue
@@ -2,26 +2,25 @@
 import { ASSET_ROOT } from "../../config/api_roots"
 import { router } from "../../router/router"
 
-function moveMenuList(categoryId: number, menuName: string): void {
+function moveMenuList(categoryId: string, menuName: string): void {
   menuName = menuName.replace(/\s+/g,"")
   router.push({name: "Menu", params: { categoryId: categoryId , menuName: menuName }})
 }
 </script>
 
 <template>
-  <div class="inline-grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
+  <div class="inline-grid gap-x-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5">
     <div
       v-for="child in children"
       :key="child.data.id"
-      class="inline-grid text-regal-blue font-semibold items-center place-items-center text-xl md:text-2xl"
+      class="inline-grid text-regal-blue font-semibold items-center place-items-center text-xl md:text-2xl cursor-pointer"
+      @click="moveMenuList(child.id, child.data.name)"
     >
-      <div
-        @click="moveMenuList(child.id, child.data.name)"
-      >
+      <div class="shadow-md">
         <a>
           <img
             :src="ASSET_ROOT + child.data.image"
-            class="md:h-80 h-60"
+            class="w-full  lg:h-96 md:h-80 h-60 aspect-square object-contain"
             alt="Menu Image"
           >
         </a>
