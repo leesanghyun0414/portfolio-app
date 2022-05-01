00be9adc320d1411db7e172457963f95a789c0c7
sanghyun.lee <lee.sanghyun0414@gmail.com>
2022-05-01 23:50:13 +0900

    Update MenuCategory Component 1. change children props required true to false

--- frontend/src/components/menu_category/MenuCategory.vue
+++ frontend/src/components/menu_category/MenuCategory.vue
@@ -9,7 +9,16 @@ const categories = useResult(result, null, (data) => data.allCategory)
 </script>
 
 <template>
-  <div class="px-4 grid grow gap-y-10">
+  <div
+    v-if="loading"
+    class="animate-pulse delay-150 rounded-0 shadow-lg bg-slate-200 grid animate-pulse  mx-auto text-sm lg:text-lg md:text-md lg:mx-0 grid-cols-none md:grid-cols-2 lg:grid-cols-3 auto-cols-fr gap-24"
+  >
+    <div class="w-screen h-screen" />
+  </div>
+  <div
+    v-else
+    class="px-4 grid grow gap-y-10"
+  >
     <div
       v-for="category in categories"
       :key="category.id"
