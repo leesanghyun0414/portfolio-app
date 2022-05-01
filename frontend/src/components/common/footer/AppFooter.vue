525ff699253b518c4c32c5bc3186d63e8e62efdf
Lee sanghyun <52747022+leesanghyun0414@users.noreply.github.com>
2022-05-01 16:15:31 +0900

    master prod pl (#31)
    * update media api endpoint
    
    * footerの実装 (#30)
    
    * footer画像追加
    
    * footerコンポーネントをApp.vueに追加
    
    * footerの実装
    
    * 不要な改行削除
    
    Co-authored-by: sonoda0529 <80201162+sonoda0529@users.noreply.github.com>

---
+++ frontend/src/components/common/footer/AppFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="flex flex-col bg-regal-blue text-white place-items-center py-10">
+    <div class="w-ful ">
+      <img
+        src="/assets/footerlogo.png"
+        alt="footerlogo"
+        class=""
+      >
+    </div>
+    <div>
+      <small>&copy; portfolio cafesquare</small>
+    </div> 
+  </footer>
+</template>
+
+
+
+<script lang="ts">
+import {defineComponent} from "vue"
+
+
+export default defineComponent({
+ name:"AppFooter",
+
+
+})
+</script>
