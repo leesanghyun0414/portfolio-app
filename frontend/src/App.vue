db79f8d5225847ddd5d5314d4614840cf5d89681
rinnmoon <kojurinnmymelo@gmail.com>
2022-05-01 16:10:24 +0900

    不要な改行削除

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,13 +2,11 @@
   <AppHeader />
   <router-view />
   <AppFooter />
-  
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue"
 import AppHeader from "./components/common/header/AppHeader.vue"
-
 import AppFooter from "./components/common/footer/AppFooter.vue"
 
 export default defineComponent({
