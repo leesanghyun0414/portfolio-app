b6dc49d5c28882fc451d82da13cc45609e4a77ad
sanghyun.lee <lee.sanghyun0414@gmail.com>
2022-05-09 18:28:06 +0900

    SECRET_KEYの隠蔽

--- backend/containers/django/config/settings.py
+++ backend/containers/django/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)e7!sv0i%m%n**)fz#l4e3-1inu4z5&zh-em+z&nt(#!b0g%f_"
+SECRET_KEY = env.get_value("SECRET_KEY", str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env.get_value("DEBUG", bool)
